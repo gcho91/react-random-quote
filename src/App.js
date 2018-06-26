0e1ae54a43d15e15d7aa7958618cf55d13f6d818
gcho91 <gcho91@gmail.com>
2018-06-26 17:59:13 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import './App.css';
+import './normalize.css';
+
+const quotes = [
+  [ ["The difference between the novice and the master is that the master has failed more times than the novice has tried. "], ["Koro Sensei"]], 
+  [["We need each other to find our own inspiration. Sometimes it's not easy."], ["Kiki"]],
+  [["Run until you can't run anymore, and then run again. Constant effort is life's greatest shortcut."], ["Genji Kamogawa"]],
+  [["It may be hard right now, but you must silence those thoughts! Stop counting only those things that you have lost! What is gone, is gone. So ask yourself this, what is there that still remains to you?"], ["Jinbei"]],
+  [["How can you move forward if you keep regretting the past?"],["Edward Elric"]],
+  [["The circumstances of one's birth are irrelevant, but it's what you do with the gift of life that determines who you are."],["Mewtwo"]],
+  [["If you don't like your destiny, don't accept it. Instead, have the courage to change it the way you want it to be!"],["Naruto Uzumaki"]]
+  ]
+
+
+  class Quote extends React.Component {
+    constructor(props){
+      super(props);
+      this.newQuote = this.newQuote.bind(this);
+      this.state = {
+        quote: '',
+        author: ''
+      }
+    }
+    
+    newQuote(){
+      let random = Math.floor((Math.random() * 6)+1);
+      
+      this.setState( ()=> {
+        return {
+          quote: quotes[random][0],
+          author: quotes[random][1]
+          //set state to quote and author
+        }
+      })
+  
+    }
+    
+    componentDidMount(){
+      this.newQuote();
+    }
+    
+    //as soon as component loads, it runs new Quote method
+    
+    render(){
+      return (
+        
+        <div className="quote-component">
+        
+        <div className="quote-card">
+          <div className="quote-text">
+            <h1>"{this.state.quote}"</h1>
+            <h3>-{this.state.author}</h3>
+          </div>
+
+
+
+
+          <div className="quote-button-container">
+          
+          
+            <button className="quote-button" onClick={this.newQuote}>   Generate A Quote</button>
+                {/* <button onClick={this.newQuote}>   Tweet</button> */}
+
+            <button className="quote-tweet quote-button">Tweet</button>
+          
+          </div>
+        </div>    
+        
+        
+        <Footer /> 
+
+
+          </div>
+      )
+    }
+  
+  }
+
+
+
+  class Footer extends Component {
+    render() {
+      return (
+        <div className="Footer">
+          <h3>Random Quote Generator</h3>
+          <p>by Gloria Cho</p>
+
+        </div>
+      );
+    }
+  }
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Quote />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
